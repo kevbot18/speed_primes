048f3045c4b83c53217d9c6688bb4d9acf171442
kevbot18 <techprophet1+git@gmail.com>
2019-08-26 17:30:18 -0400

    Increase max size of input
    With memory usage halved the program can count the number of primes below 2^32 more quickly and reasonably.

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,8 @@ use std::env;
 fn main() {
 	// takes user imput and puts it in a vector
 	let input: Vec<String> = env::args().collect();
-	// takes first element in input vector and converts it to a u32 for prime number program
-	let num: u32 = input[1].parse::<u32>().unwrap();
+	// takes first element in input vector and converts it to a u64 for prime number program
+	let num: u64 = input[1].parse::<u64>().unwrap();
 	// defaults to NOT showing all primes, and only counting them
 	let list_primes = if input.len() > 2 { // checks if there are any more args
 		match input[2].as_ref() {	// if the args match, show all primes
@@ -53,7 +53,7 @@ fn main() {
 	println!("\n{} primes", num_primes(num, list_primes));
 }
 
-fn num_primes(num: u32, show_primes: bool) -> u32 {
+fn num_primes(num: u64, show_primes: bool) -> u32 {
 	let mut count: u32 = 0;
 	let elements = if num % 16 == 0 { num / 16 } else { num / 16 + 1 };
 
@@ -66,7 +66,6 @@ fn num_primes(num: u32, show_primes: bool) -> u32 {
 	let root = isqrt(num);
 	let mut i:usize = 3;
 	while i < (root + 1) as usize { // only check to the square
-		if i % 2 == 0 { continue; }
 		if is_prime[i / 16] >> ((i/2) % 16) & 1 == 1 { // checks if bit of current number (i) is high
 			let mut k: usize = i;
 			while k * i <= num as usize { // only check to number
@@ -80,8 +79,7 @@ fn num_primes(num: u32, show_primes: bool) -> u32 {
 	// remove ones above given number (sets bits to low so they are ignored when counting)
 	let mut i = num + 1;
 	while i < elements * 16 { // only worry about values greater than input num
-		if i % 2 == 0 { continue; }
-		is_prime[(elements - 1) as usize] &= !(2u8.pow(i/2 % 8) as u8); // sets bit to low using AND NOT (see above)
+		is_prime[(elements - 1) as usize] &= !(1u8<<(i/2 % 8) as u8); // sets bit to low using AND NOT (see above)
 		i += 2;
 	}
 
@@ -112,7 +110,7 @@ fn num_primes(num: u32, show_primes: bool) -> u32 {
 
 
 // recursive integer square root function found on Wikipedia
-fn isqrt(n: u32) -> u32 {
+fn isqrt(n: u64) -> u64 {
 	if n < 2 {
 		return n;
 	} else {
